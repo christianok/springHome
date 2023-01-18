9f831991f111d59be26389fb661bec82658f90e8
zhuoli <zhuoliforever@163.com>
2023-01-18 21:10:17 +0800

    test: test cases

---
+++ dubbo-consumer/src/test/java/com/minbo/dubbo/consumer/CollectionTests.java
@@ -0,0 +1,17 @@
+package com.minbo.dubbo.consumer;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
+public class CollectionTests {
+
+    @Test
+    public void testHashMap(){
+        Map<String, String> map = new HashMap<String, String>();
+
+    }
+}
--- dubbo-consumer/src/test/java/com/minbo/dubbo/consumer/DubboConsumerApplicationTests.java
+++ dubbo-consumer/src/test/java/com/minbo/dubbo/consumer/DubboConsumerApplicationTests.java
@@ -1,6 +1,7 @@
 package com.minbo.dubbo.consumer;
 
 import com.sun.scenario.animation.shared.PulseReceiver;
+import lombok.Data;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +32,61 @@ public class DubboConsumerApplicationTests {
 		List<String> list = new ArrayList<String>();
 		list.add("java");
 		list.add("learning");
-		System.out.println(list.size());
-		//Map<String, String> hashmap = new HashMap<String, String>();
+		//System.out.println(list.size());
+		Iterator<String> iterator = list.iterator();
+		while (iterator.hasNext()){
+			String next = iterator.next();
+			System.out.println(next);
+		}
+
+	}
+
+	@Test
+	@SuppressWarnings({"all"})
+	public void testList(){
+		List<Book> list = new ArrayList<Book>();
+		list.add(new Book("caocao", "duangexing"));
+		list.add(new Book("caoxuqin", "hongloumeng"));
+		list.add(new Book("caozhi", "luoshenfu"));
+
+		//System.out.println(list.size());
+		Iterator<Book> iterator = list.iterator();
+		while (iterator.hasNext()){
+			Book next = iterator.next();
+			System.out.println(next.toString());
+		}
+	}
+
+	@Test
+	public void testListOrder(){
+		List<Book> books = new LinkedList<Book>();
+		List<Book> list = new ArrayList<Book>();
+		list.add(new Book("caocao", "duangexing", 10));
+		list.add(new Book("caoxuqin", "hongloumeng", 9));
+		list.add(new Book("caozhi", "luoshenfu", 8));
+
+	}
+
+	@Data
+	class Book{
+		String author;
+		String book;
+		Integer price;
+
+		public Book(String author, String book){
+			this.author = author;
+			this.book = book;
+		}
+
+		public Book(String author, String book, Integer price){
+			this.author = author;
+			this.book = book;
+			this.price = price;
+		}
+
+		@Override
+		public String toString(){
+			return  this.author + " : " +  this.book + " : " + (this.price == null ? "" : this.price);
+		}
 	}
 }
